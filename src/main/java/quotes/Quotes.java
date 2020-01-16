07d152e40f8bbde988f0e0577c83fcfd87066a4f
Mattäus Sebastian Isenberg <mattaus@gmx.com>
2020-01-16 11:11:06 -0800

    Commit of working solution

--- src/main/java/quotes/Quotes.java
+++ src/main/java/quotes/Quotes.java
@@ -1,4 +1,29 @@
 package quotes;
 
+import java.security.PublicKey;
+
 public class Quotes {
+
+    String[] tags;
+    String author;
+    String likes;
+    String text;
+
+    public Quotes(String[] tags, String author, String likes, String text)
+    {
+        this.tags = tags;
+        this.author = author;
+        this.likes = likes;
+        this.text = text;
+    }
+
+    public void randomQuote()
+    {
+
+    }
+
+    public String toString()
+    {
+        return String.format(this.text + "\n" + this.author + "\n");
+    }
 }
